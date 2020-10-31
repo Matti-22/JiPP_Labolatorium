540b00757d5c4c1ee516cc07e1bdbf99f1e2c6dd
Matti-22 <Matti-22@users.noreply.github.com>
2020-10-31 18:49:11 +0100

    Dokonczenie programu

--- lab2/z2.cpp
+++ lab2/z2.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-void zamiana(int &ra,int &rb)
+void zamiana(int &ra, int &rb)
 {
-    
+    int pom = ra;
+    ra = rb;
+    rb = pom;
 }
 
-int main(){
+int main()
+{
     int a, b;
     cout << "Podaj pierwsza liczbe:" << endl;
     cin >> a;
     cout << "Podaj druga liczbe:" << endl;
-    cin >> b; 
+    cin >> b;
 
-
+    cout << "Liczby przed zamieniniem a = " << a << " b = " << b << endl;
+    zamiana(a, b);
+    cout << "Liczby po zamieniniem a = " << a << " b = " << b << endl;
 }
