06dce1b79b75fb0c80db7e441c02735a23790ffe
Matti-22 <Matti-22@users.noreply.github.com>
2020-10-31 17:01:28 +0100

    pobieranie danych

--- z1.cpp
+++ z1.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
-int main(int argc, char *argv[]){
-    
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int a, b;
+    int *w1 = NULL, *w2 = NULL;
+    cout << "Podaj pierwsza liczbe:" << endl;
+    cin >> a;
+    cout << "Podaj druga liczbe:" << endl;
+    cin >> b;
+
+
 }
