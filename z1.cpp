d3634461dcaa6b3caad5aeaec78d1fb678bb84ec
Matti-22 <Matti-22@users.noreply.github.com>
2020-10-31 18:00:04 +0100

    skonczenie programu

--- z1.cpp
+++ z1.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+void wieksza(int *w1, int *w2)
+{
+    int pom;
+    if (*w1 < *w2)
+    {
+        pom = *w1;
+        *w1 = *w2;
+        *w2 = pom;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int a, b;
@@ -11,7 +22,9 @@ int main(int argc, char *argv[])
     cout << "Podaj druga liczbe:" << endl;
     cin >> b;
 
-    *w1 = a;
-    *w2 = b;    
+    w1 = &a;
+    w2 = &b;
 
+    wieksza(w1, w2);
+    cout << "Liczba " << *w1 << " Jest wieksza lub rowna od liczby " << *w2 << endl;
 }
