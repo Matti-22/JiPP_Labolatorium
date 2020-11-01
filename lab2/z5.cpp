cbd29321c602d00f5af15c5c881d9f1d6e8a4b39
Matti-22 <Matti-22@users.noreply.github.com>
2020-11-01 16:27:31 +0100

    dodatnie funkcji swap

--- lab2/z5.cpp
+++ lab2/z5.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+//funkcja zamienia ze soba wartosci a i b uąywając wskaźników 
+template <class T>
+void swap(T *a, T *b)
+{
+    T pom = *a;
+    *a = *b;
+    *b = pom;
+}
+
 int main()
 {
     int a, b;
@@ -15,5 +24,14 @@ int main()
     cout << "Podaj drugie slowo:" << endl;
     cin >> sb;
 
-    cout << "a = " << a << " b = " << b << " sa = " << sa << " sb = " << sb << endl;
+    //wyświetlanie efektów programu
+    cout << "Liczby przed zamieniniem a = " << a << " b = " << b << endl;
+    swap(&a,&b);
+    cout << "Liczby po zamieniniu a = " << a << " b = " << b << endl;
+
+    cout << "slowa przed zamieniniem sa = " << sa << " sb = " << sb << endl;
+    swap(&sa,&sb);
+    cout << "slowa po zamieniniu sa = " << sa << " sb = " << sb << endl;
+
+    
 }
