dbd6366f3f8b713475dbbe02e5838a4167e185fa
Matti-22 <Matti-22@users.noreply.github.com>
2020-11-01 15:37:34 +0100

    testy przypisania zmiennych

---
+++ lab2/z5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    string sa, sb;
+    cout << "Podaj pierwsza liczbe:" << endl;
+    cin >> a;
+    cout << "Podaj druga liczbe:" << endl;
+    cin >> b;
+    cout << "Podaj pierwsze slowo:" << endl;
+    cin >> sa;
+    cout << "Podaj drugie slowo:" << endl;
+    cin >> sb;
+
+    cout << "a = " << a << " b = " << b << " sa = " << sa << " sb = " << sb << endl;
+}
