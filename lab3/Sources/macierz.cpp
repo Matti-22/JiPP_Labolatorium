a53cb712c8994a69143e230a5d17d3b74fa32297
Matti-22 <Matti-22@users.noreply.github.com>
2020-11-12 18:39:40 +0100

    dodanie podfolderow

---
+++ lab3/Sources/macierz.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include "Headers\macierz.h"
+using namespace std;
---
+++ lab3/Sources/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Headers\macierz.h"
+using namespace std;
+
+int main()
+{
+    return 0;
+}
