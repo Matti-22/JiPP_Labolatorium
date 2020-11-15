a91ba3f871974d25acbed233af48f262d9efc1bb
Matti-22 <Matti-22@users.noreply.github.com>
2020-11-15 22:44:44 +0100

    modyfikacja maina - zmienne klasy od teraz prywatne

--- lab3/Sources/main.cpp
+++ lab3/Sources/main.cpp
@@ -4,24 +4,30 @@ using namespace std;
 
 int main()
 {
+    //Tworzenie nowych macierzy i pobarnie liczby ich kolumn i wierszy
     Macierz m1(3);
+    int k1 = m1.cols();
+    int w1 = m1.rows();
     Macierz m2(3);
+    int k2 = m2.cols();
+    int w2 = m2.rows();
 
     double pom;
-    for (int i = 0; i < m1.wiersze; i++)
+
+    cout << "Prosze podac wartosci pierwszej macierzy:" << endl;
+    for (int i = 0; i < w1; i++)
     {
-        for (int j = 0; j < m1.kolumny; j++)
+        for (int j = 0; j < k1; j++)
         {
             cin >> pom;
             m1.set(i, j, pom);
         }
     }
 
-    cout << "KOLEJNA" << endl;
-
-    for (int i = 0; i < m2.wiersze; i++)
+    cout << "Prosze podac wartosci drugiej macierzy:" << endl;
+    for (int i = 0; i < w2; i++)
     {
-        for (int j = 0; j < m2.kolumny; j++)
+        for (int j = 0; j < k2; j++)
         {
             cin >> pom;
             m2.set(i, j, pom);
@@ -30,9 +36,8 @@ int main()
 
     Macierz m3 = m1.multiply(m2);
 
-    int k = m1.cols();
     cout << endl
-         << "liczba kolumn: " << k << endl;
+         << "liczba wierszy m1: " << w1 << endl;
     /*
     m1.set(1, 2, 13);
     double pom = m1.get(-1, 2);
