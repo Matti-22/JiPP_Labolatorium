d965f14a39d580282a8ec93adff3d05e82c4fc02
Matti-22 <Matti-22@users.noreply.github.com>
2021-01-12 12:07:54 +0100

    wczytanie lab3

---
+++ lab7/include/macierz.h
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Macierz
+{
+    int wiersze, kolumny;
+    double **tablica;
+
+public:
+    //Konstruktor służący do tworzenia macierzy o x wierszach i y kolumnach
+    Macierz(int w, int k);
+    //Konstruktor służący do tworzenia macierzy kwadratowej
+    Macierz(int r);
+    //Konstruktor czytający dane na temat macierzy z pliku, przyjmujący jako argument ścieżkę do tego pliku
+    Macierz(string path);
+    //Destruktor
+    ~Macierz();
+
+    //Metoda ustawia wartość elementu (n,m) na val
+    void set(int n, int m, double val);
+
+    //Metoda pobierająca element (n,m)
+    double get(int n, int m);
+
+    //Metoda przyjmująca jako argument inną macierz zwraca jako wynik nową macierz będącą sumą bieżacej macierzy oraz macierzy przekazanej jako argument
+    Macierz add(Macierz& m2);
+
+    //Metoda przyjmująca jako argument inną macierz i zwracająca jako wynik nową macierz będącą różnicą bieżącej macierzy oraz macierzy przekazanej jako argument
+    Macierz subtract(Macierz& m2);
+
+    //Metoda przyjmująca jako argument inną macierz i zwracająca jako wynik nową macierz będącą iloczynem bieżącej macierzy oraz macierzy przekazanej jako argument
+    Macierz multiply(Macierz& m2);
+
+    //Metoda zwracająca liczbę kolumn macierzy
+    int cols();
+
+    //Metoda zwracająca liczbę wierszy macierzy
+    int rows();
+
+    //Metoda ma za zadanie wyświetlić macierz na ekranie
+    void print();
+
+    //Metoda zapisująca macierz w pliku, w pierwszym wierszu pliku jej wymiary (liczbę kolumn i wierszy) oraz jej zawartość (każdy wiersz macierzy w osobnym wierszu pliku)
+    void store(string filename, string path);
+    
+};
